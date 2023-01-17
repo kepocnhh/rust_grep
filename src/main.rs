8d1dfedc7138b502c55d1229262d8b91f676735f
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-18 00:10:02 +0300

    Accepting Command Line Arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("rust grep")
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.is_empty() {
+        println!("No args.");
+        return;
+    }
+
+    println!("Args:");
+    for (index, it) in args.iter().enumerate() {
+        println!("{index}] {it}");
+    }
 }
