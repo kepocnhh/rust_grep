ba01811387f97c0a582f16880d1d49e7935cd8ba
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-18 00:41:06 +0300

    Reading a File

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
+fn print_help() {
+    println!("Usage: rust_grep [options...]");
+    println!("-f <file_path>\tRead from file by <file_path>");
+    println!("--help\t\tGet help");
+}
+
+fn on_file_path(file_path: &String) {
+    let contents = std::fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    todo!("contents: {contents}");
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.is_empty() {
-        println!("No args.");
-        return;
+    let len = args.len();
+    if len < 2 {
+        panic!("Args size error!");
     }
 
-    println!("Args:");
-    for (index, it) in args.iter().enumerate() {
-        println!("{index}] {it}");
+    if len == 2 {
+        if args[1] == "--help" {
+            print_help();
+            return;
+        }
     }
+
+    if len == 3 {
+        let flag = args[1].as_str();
+        match flag {
+            "-f" => on_file_path(&args[2]),
+            _ => panic!("Flag \"{flag}\" is not supported!")
+        }
+    }
+
+    print_help();
 }
